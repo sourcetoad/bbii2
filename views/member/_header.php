55a411bf00091d63657ad1ced09571c8ce37316b
David E <me@davidjeddy.com>
2015-05-14 17:12:14 -0400

    FIX module JS/CSS/IMG now load correctly

--- views/member/_header.php
+++ views/member/_header.php
@@ -16,11 +16,11 @@ $assets = AppAsset::register($this);
 		<div class = "bbii-profile-box">
 		<?php 
 			if($messages) {
-				echo Html::a(Html::img($assets->baseUrl.'newmail.png', Yii::t('BbiiModule.bbii', 'new messages'), array('title' => $messages . ' ' . Yii::t('BbiiModule.bbii', 'new messages'),'style' => 'vertical-align:bottom;')), array('message/inbox')); 
+				echo Html::a(Html::img($assets->baseUrl.'/images/newmail.png', array('title' => $messages . ' ' . Yii::t('BbiiModule.bbii', 'new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox')); 
 			} else {
-				echo Html::a(Html::img($assets->baseUrl.'mail.png', Yii::t('BbiiModule.bbii', 'no new messages'), array('title' => Yii::t('BbiiModule.bbii', 'no new messages'),'style' => 'vertical-align:bottom;')), array('message/inbox')); 
+				echo Html::a(Html::img($assets->baseUrl.'/images/mail.png', array('title' => Yii::t('BbiiModule.bbii', 'no new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox')); 
 			}
-			echo ' | ' . Html::a(Html::img($assets->baseUrl.'settings.png', Yii::t('BbiiModule.bbii', 'My settings'), array('title' => Yii::t('BbiiModule.bbii', 'My settings'),'style' => 'vertical-align:bottom;')), array('member/view', 'id'  => Yii::$app->user->id)); 
+			echo ' | '; echo Html::a(Html::img($assets->baseUrl.'/images/settings.png', array('title' => Yii::t('BbiiModule.bbii', 'My settings'), 'style' => 'vertical-align:bottom;')), array('member/view', 'id'  => Yii::$app->user->id)); 
 		?>
 		</div>
 	<?php endif; ?>
