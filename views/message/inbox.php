99f88a8ba40a7b972209753da612067a53c4bb5c
David E <me@davidjeddy.com>
2015-05-15 11:27:58 -0400

    VW message/inbox now renders

--- views/message/inbox.php
+++ views/message/inbox.php
@@ -1,7 +1,7 @@
 <?php
 
+use yii\grid\GridView;
 use yii\helpers\Html;
-
 use yii\web\UrlManager;
 
 use frontend\modules\bbii\AppAsset;
@@ -27,7 +27,8 @@ $item = array(
 	
 	<div class = "progress"><div class = "progressbar" style = "width:<?php echo ($count['inbox'] < 100)?$count['inbox']:100; ?>%"> </div></div>
 
-	<?php $this->widget('zii.widgets.grid.CGridView', array(
+	<?php // @depricated 2.1.5 Kept for referance
+	/*$this->widget('zii.widgets.grid.CGridView', array(
 		'id' => 'inbox-grid',
 		'dataProvider' => $model->search(),
 		'rowCssClassExpression' => '($data->read_indicator)?"":"unread"',
@@ -67,8 +68,49 @@ $item = array(
 				)
 			),
 		),
+	));*/ ?>
+
+	<?php echo GridView::widget(array(
+		'columns'      => array(
+			array(
+				'header' => 'sendfrom',
+				'value'  => '$data->sender->member_name'
+			),
+			'subject',
+			array(
+				'header' => 'create_time',
+				'value'  => 'DateTimeCalculation::long($data->create_time)',
+			),
+			array(
+				'header' => 'type',
+				'value'  => '($data->type)?Yii::t("bbii", "notification"):Yii::t("bbii", "message")',
+			),
+			array(
+				'buttons'  => array(
+					'view' => array(
+						'click'    => 'js:function() { viewMessage($(this).attr("href"), "' . Yii::$app->urlManager->createAbsoluteUrl('message/view') .'");return false; }',
+						'imageUrl' => $assets->baseUrl.'view.png',
+						'url'      => '$data->id',
+					),
+					'reply' => array(
+						'imageUrl' => $assets->baseUrl.'reply.png',
+						'label'    => Yii::t('BbiiModule.bbii','Reply'),
+						'options'  => array('style' => 'margin-left:5px;'),
+						'url'      => 'array("reply", "id" => $data->id)',
+					),
+					'delete' => array(
+						'imageUrl' => $assets->baseUrl.'/images/delete.png',
+						'options'  => array('style' => 'margin-left:5px;'),
+					),),
+				'class'    => 'yii\grid\ActionColumn',
+				'template' => '{view}{reply}{delete}',
+			)),
+		'dataProvider'          => $model->search(),
+		'id'                    => 'inbox-grid',
+		// @todo Figure out the Yii2 version of this logic - DJE : 2015-05-15
+		//'rowCssClassExpression' => '($data->read_indicator)?"":"unread"',
 	)); ?>
-	
+
 	<div id = "bbii-message"></div>
 
 </div>
