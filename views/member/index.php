0ac32d005b8b0e5b1e741b40f72488e8caa267c9
David E <me@davidjeddy.com>
2015-05-18 12:55:44 -0400

    VW member/index GridView iterated on

--- views/member/index.php
+++ views/member/index.php
@@ -7,6 +7,7 @@ use frontend\modules\bbii\models\BbiiMembergroup;
 use yii\grid\GridView;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use yii\web\UrlManager;
 
 use frontend\modules\bbii\AppAsset;
 $assets = AppAsset::register($this);
@@ -14,20 +15,20 @@ $assets = AppAsset::register($this);
 /* @var $this ForumController */
 
 $this->context->bbii_breadcrumbs = array(
-	array('url' => 'forum/index', 'label' => Yii::t('BbiiModule.bbii', 'Forum')),
+	//Yii::t('BbiiModule.bbii', 'Forum') => array('forum/index'),
 	Yii::t('BbiiModule.bbii', 'Members'),
 );
 
 $approvals = BbiiPost::find()->unapproved()->count();
-$reports   = BbiiMessage::find()->report()->count();
+$reports = BbiiMessage::find()->report()->count();
 
 $item = array(
-	array('label' => Yii::t('BbiiModule.bbii', 'Forum'), 							'url' => array('forum/index')),
-	array('label' => Yii::t('BbiiModule.bbii', 'Members'), 							'url' => array('member/index')),
-	array('label' => Yii::t('BbiiModule.bbii', 'Approval'). ' (' . $approvals . ')','url' => array('moderator/approval'), 	'visible' => $this->context->isModerator()),
-	array('label' => Yii::t('BbiiModule.bbii', 'Reports'). 	' (' . $reports . ')', 	'url' => array('moderator/report'), 	'visible' => $this->context->isModerator()),
-	array('label' => Yii::t('BbiiModule.bbii', 'Posts'), 							'url' => array('moderator/admin'), 		'visible' => $this->context->isModerator()),
-	array('label' => Yii::t('BbiiModule.bbii', 'Blocked IP'), 						'url' => array('moderator/ipadmin'), 	'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Forum'), 'url' => array('forum/index')),
+	array('label' => Yii::t('BbiiModule.bbii', 'Members'), 'url' => array('member/index')),
+	array('label' => Yii::t('BbiiModule.bbii', 'Approval'). ' (' . $approvals . ')', 'url' => array('moderator/approval'), 'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Reports'). ' (' . $reports . ')', 'url' => array('moderator/report'), 'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Posts'), 'url' => array('moderator/admin'), 'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Blocked IP'), 'url' => array('moderator/ipadmin'), 'visible' => $this->context->isModerator()),
 );
 ?>
 <div id = "bbii-wrapper">
@@ -38,7 +39,7 @@ $item = array(
 		'id'           => 'member-grid',
 		'dataProvider' => $model->search(),
 		'filter'       => $model,
-		'columns' => array(
+		/*'columns' => array(
 			array(
 				'header' => 'Avatar',
 				'type' => 'image',
@@ -60,43 +61,37 @@ $item = array(
 			),
 			array(
 				'name' => 'group_id',
-				'filter' => ArrayHelper::map(BbiiMembergroup::find()->findAll(), 'id', 'name'),
+				'filter' => Html::listData(BbiiMembergroup::find()->findAll(), 'id', 'name'),
 				'value' => '(isset($data->group))?$data->group->name:""',
 			),
 		),
 	));*/ ?>
 
-	<?php echo GridView::widget(array(
-		'columns'      => array(
-			array(
-				'format'  => 'image',
-				'label'   => 'Avatar',
-				'options' => array('class' => 'img30'),
-				'value'   => '(isset($data->avatar)) ? Yii::$app->request->baseUrl.$this->module->avatarStorage.\'/$data->avatar\' : $assets->baseUrl.\'/images/empty.jpeg\')',
-			),
-			array(
-				'format' => 'raw',
-				'label'  => 'Name',
-				'value'  => 'Html::a(Html::encode($data->member_name), array("member/view", "id" => $data->id))',
-			),
-			array(
-				'label' => Yii::t('BbiiModule.bbii', 'Joined'),
-				'value' => 'DateTimeCalculation::full($data->first_visit)',
-			),
-			array(
-				'label' => Yii::t('BbiiModule.bbii', 'Last visit'),
-				'value' => 'DateTimeCalculation::full($data->last_visit)',
-			),
-			array(
-				'filter' => ArrayHelper::map(BbiiMembergroup::find()->all(), 'id','name'),
-				'label'  => 'Group',
-				'value'  => '(isset($data->group))?$data->group->name:""',
-			)),
+    <?= GridView::widget([
+		'columns'      => [
+            ['class' => 'yii\grid\SerialColumn'],
+ 
+            'avatar',
+			[
+				'attribute' => 'member_name',
+				'value'     => 'member_name',
+			],
+			[
+				'attribute' => 'first_visit',
+				'value'     => 'first_visit',
+			],
+			[
+				'attribute' => 'last_visit',
+				'value'     => 'last_visit',
+			],
+            'group_id',
+
+            ['class' => 'yii\grid\ActionColumn']
+           ],
 		'dataProvider' => $model->search(),
 		'filterModel'  => $model,
-		'id'           => 'member-grid',
-	)); ?>
-
+    ]); ?>
+	
 	<?php echo $this->render('_footer'); ?>
 	<div id = "bbii-copyright"><a href = "http://www.yiiframework.com/extension/bbii/" target = "_blank" title = "&copy; 2013-2014 <?php echo Yii::t('BbiiModule.bbii','version') . ' ' . $this->context->module->version; ?>">BBii forum software</a></div>
 </div>
