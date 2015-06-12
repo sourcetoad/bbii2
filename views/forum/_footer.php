2a37acf977308751235338d95196cc69364fc39c
Phil Sanders <philsanders79@gmail.com>
2015-06-11 21:08:21 -0400

    Refactored "Who is Online" panel

--- views/forum/_footer.php
+++ views/forum/_footer.php
@@ -18,22 +18,25 @@ $present = BbiiSession::find()->present()->count();
     <div class="well">
         <div class="clearfix">
             <div class="col-sm-12 col-md-8 online">
-                <h5>
-                    <?php echo Yii::t('BbiiModule.bbii','{0} guest(s) and {1} active member(s)', array(($present - $members) > 0 ?: 0,$members));?>
-                    <small><?php echo Yii::t('BbiiModule.bbii','(in the past 15 minutes)');?></small>
-                </h5>
-
-                <?php $members = BbiiMember::find()->present()->show()->findAll();
-                    foreach($members as $member) {
-                        echo Html::a($member->member_name, array('member/view', 'id' => $member->id), array('style' => 'color:#'.$member->group->color)) . '&nbsp;';
-                    }
-                    $spiders = BbiiSpider::find()->present()->findAll();
-                    foreach($spiders as $spider) {
-                        echo Html::a($spider->name, $spider->url, array('class' => 'spider','target' => '_new')) . '&nbsp;';
-                    }
-                ?>
-                <?php echo Yii::t('BbiiModule.bbii','({0} anonymous member(s))', array(BbiiMember::find()->hidden()->present()->count())); ?>
-
+                <h4>
+                    <?php echo Yii::t('BbiiModule.bbii','Who\'s Online'); ?>
+                </h4>
+                <div class="col col-sm-12 panel">
+                    <h5>
+                        <?php echo Yii::t('BbiiModule.bbii','{0} guest(s) and {1} active member(s)', array(($present - $members) > 0 ?: 0,$members));?>
+                        <small><?php echo Yii::t('BbiiModule.bbii','(in the past 15 minutes)');?></small>
+                    </h5>
+                    <?php $members = BbiiMember::find()->present()->show()->findAll();
+                        foreach($members as $member) {
+                            echo Html::a($member->member_name, array('member/view', 'id' => $member->id), array('style' => 'color:#'.$member->group->color)) . '&nbsp;';
+                        }
+                        $spiders = BbiiSpider::find()->present()->findAll();
+                        foreach($spiders as $spider) {
+                            echo Html::a($spider->name, $spider->url, array('class' => 'spider','target' => '_new')) . '&nbsp;';
+                        }
+                    ?>
+                    <?php echo Yii::t('BbiiModule.bbii','({0} anonymous member(s))', array(BbiiMember::find()->hidden()->present()->count())); ?>
+                </div>
             </div>
             <div class="col-sm-12 col-md-4 statistics">
                 <h4>
