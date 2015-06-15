bb09a2cc3eae70abbc9020414474276743381096
Phil Sanders <philsanders79@gmail.com>
2015-06-15 12:58:50 -0400

    Removing unecessary button classes

--- views/member/view.php
+++ views/member/view.php
@@ -22,31 +22,19 @@ $this->context->bbii_breadcrumbs = array(
 $item = array(
 	array(
       'label'   => Yii::t('BbiiModule.bbii', 'Forum'),
-      'url'     => array('forum/index'),
-      'options' => array(
-        'class' =>'btn btn-warning'
-      )
+      'url'     => array('forum/index')
     ),
 	array(
       'label' => Yii::t('BbiiModule.bbii', 'Members'),
-      'url'   => array('member/index'),
-      'options' => array(
-        'class' =>'btn btn-warning'
-      )
+      'url'   => array('member/index')
     ),
 	array(
       'label' => Yii::t('BbiiModule.bbii', 'Approval'),
-      'url'   => array('moderator/approval'), 'visible' => $this->context->isModerator(),
-      'options' => array(
-        'class' =>'btn btn-warning'
-      )
+      'url'   => array('moderator/approval'), 'visible' => $this->context->isModerator()
     ),
 	array(
       'label' => Yii::t('BbiiModule.bbii', 'Posts'),
-      'url' => array('moderator/admin'), 'visible' => $this->context->isModerator(),
-      'options' => array(
-        'class' =>'btn btn-warning'
-      )
+      'url' => array('moderator/admin'), 'visible' => $this->context->isModerator()
     ),
 );
 
