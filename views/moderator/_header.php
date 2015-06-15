7f633f6a85de764d87c5d95a8133f58d07951681
Phil Sanders <philsanders79@gmail.com>
2015-06-15 12:58:03 -0400

    Refactored menu classes with bootstrap

--- views/moderator/_header.php
+++ views/moderator/_header.php
@@ -15,28 +15,27 @@ $assets = AppAsset::register($this);
 <div id = "bbii-header">
 	<?php if (!Yii::$app->user->isGuest): ?>
 	<?php $messages = BbiiMessage::find()->inbox()->unread()->count('sendto = '.Yii::$app->user->id); ?>
-		<div class = "pull-right">
+		<div class = "btn-group pull-right">
 		<?php 
 			if ($messages) {
-				echo Html::a(Html::img($assets->baseUrl.'/images/newmail.png', array('title' => $messages . ' ' . Yii::t('BbiiModule.bbii', 'new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox'),array('class'=>'btn btn-primary'));
+				echo Html::a(Html::img($assets->baseUrl.'/images/newmail.png', array('title' => $messages . ' ' . Yii::t('BbiiModule.bbii', 'new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox'),array('class'=>'btn btn-default'));
 			} else {
-				echo Html::a(Html::img($assets->baseUrl.'/images/mail.png', array('title' => Yii::t('BbiiModule.bbii', 'no new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox'),array('class'=>'btn btn-primary'));
+				echo Html::a(Html::img($assets->baseUrl.'/images/mail.png', array('title' => Yii::t('BbiiModule.bbii', 'no new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox'),array('class'=>'btn btn-default'));
 			}
-			echo Html::a(Html::img($assets->baseUrl.'/images/settings.png', array('title' => Yii::t('BbiiModule.bbii', 'My settings'), 'style' => 'vertical-align:bottom;')), array('member/view', 'id'  => Yii::$app->user->id),array('class'=>'btn btn-primary'));
-			echo Html::a(Html::img($assets->baseUrl.'/images/moderator.png', array('title' => Yii::t('BbiiModule.bbii', 'Moderate'), 'style' => 'vertical-align:bottom;')), array('moderator/approval'),array('class'=>'btn btn-primary'));
-			echo Html::a(Html::img($assets->baseUrl.'/images/config.png', array('title' => Yii::t('BbiiModule.bbii', 'Forum settings'), 'style' => 'vertical-align:bottom;')), array('setting/index'),array('class'=>'btn btn-primary'));
+			echo Html::a(Html::img($assets->baseUrl.'/images/settings.png', array('title' => Yii::t('BbiiModule.bbii', 'My settings'), 'style' => 'vertical-align:bottom;')), array('member/view', 'id'  => Yii::$app->user->id),array('class'=>'btn btn-default'));
+			echo Html::a(Html::img($assets->baseUrl.'/images/moderator.png', array('title' => Yii::t('BbiiModule.bbii', 'Moderate'), 'style' => 'vertical-align:bottom;')), array('moderator/approval'),array('class'=>'btn btn-default'));
+			echo Html::a(Html::img($assets->baseUrl.'/images/config.png', array('title' => Yii::t('BbiiModule.bbii', 'Forum settings'), 'style' => 'vertical-align:bottom;')), array('setting/index'),array('class'=>'btn btn-default'));
 		?>
 		</div>
 	<?php endif; ?>
 	<h2><?php echo $this->context->module->forumTitle; ?></h2>
     <br />
-	<table style = "margin:0;"><tr><td style = "padding:0;">
-		<div id = "bbii-menu">
-		<?php  echo Nav::widget([
-		    'items' => $item,
-		]); ?>
-		</div>
-	</td></tr></table>
+    <div id = "nav" class="clearfix">
+    <?php  echo Nav::widget([
+        'items' => $item,
+        'options' => array('class'=>'nav nav-pills')
+    ]); ?>
+    </div>
 </div>
 <?php /*if (isset($this->context->bbii_breadcrumbs)):?>
 	<?php echo Breadcrumbs::widget(array(
