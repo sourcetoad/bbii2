dae0d3c4863091da28bd6f38a09161978ce02501
Phil Sanders <philsanders79@gmail.com>
2015-06-15 11:00:51 -0400

    I

--- views/setting/template/_header.php
+++ views/setting/template/_header.php
@@ -20,12 +20,13 @@ $item = array(
 ?>
 <div id = "bbii-header">
 	<?php if (!Yii::$app->user->isGuest): ?>
-		<div class = "bbii-profile-box">
+		<div class = "btn btn-primary pull-right">
 		<?php echo Html::a(Yii::t('BbiiModule.bbii', 'Forum'), array('forum/index')); ?>
 		</div>
 	<?php endif; ?>
-	<div class = "bbii-title"><?php echo Yii::t('BbiiModule.bbii', 'Forum settings'); ?></div>
-	<table style = "margin:0;"><tr><td style = "padding:0;">
+	<h2><?php echo Yii::t('BbiiModule.bbii', 'Forum settings'); ?></h2>
+    <br />
+	<table><tr><td style = "padding:0;">
 		<div id = "bbii-menu">
 		<?php  echo Nav::widget([
 		    'items' => $item,
