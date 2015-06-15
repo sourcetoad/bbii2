595977107b2676cae60430e8ae7ad0309b14b246
Phil Sanders <philsanders79@gmail.com>
2015-06-15 10:47:05 -0400

    Iteration of topic header title markup

--- views/forum/topic.php
+++ views/forum/topic.php
@@ -51,11 +51,9 @@ $item = array(
 <div id = "bbii-wrapper">
 	<?php echo $this->render('_header', array('item' => $item)); ?>
 	
-	<div class = "forum-category center">
-		<div class = "header2">
-			<?php echo $topic->title; ?>
-		</div>
-	</div>
+	<h3>
+        <?php echo $topic->title; ?>
+    </h3>
 	
 	<?php if (!Yii::$app->user->isGuest && $this->context->module->userMailColumn && $this->context->module->allowTopicSub) { ?>
 		<?php if ($this->isWatching($topic->id)) { ?>
