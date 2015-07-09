27c994fa4e8e1fa3d757d494b6377120908cfdfa
Phil Sanders <philsanders79@gmail.com>
2015-07-09 19:00:42 -0400

    New iteration of forum list responsive display

--- views/forum/_forum.php
+++ views/forum/_forum.php
@@ -29,7 +29,7 @@ if (!$model->public) {
 
 <?php if ($model->type) { ?>
 <div class = "forum">
-	<div class = "forum-cell <?php echo $image; ?>"></div>
+	<div class = "forum-cell <?php echo $image; ?> hidden-xs"></div>
 	<div class = "forum-cell main">
 		<div class = "header2">
 			<?php echo Html::a(Html::encode($model->name), array('forum', 'id' => $model->id)); ?>
@@ -38,21 +38,27 @@ if (!$model->public) {
 			<?php echo Html::encode($model->subtitle); ?>
 		</div>
 	</div>
-	<div class = "forum-cell center">
+	<div class = "forum-cell forum-posts center">
 		<?php echo Html::encode($model->num_posts); ?><br>
 		<?php echo Html::encode($model->getAttributeLabel('num_posts')); ?>
 	</div>
-	<div class = "forum-cell center">
+	<div class = "forum-cell forum-posts center">
 		<?php echo Html::encode($model->num_topics); ?><br>
 		<?php echo Html::encode($model->getAttributeLabel('num_topics')); ?>
 	</div>
-	<div class = "forum-cell last-cell">
+	<div class = "forum-cell forum-date last-cell">
 		<?php
 		if (is_numeric($model->last_post_id) && !empty($model->lastPost)) {
+			//echo Html::a(Html::img($assets->baseUrl.'/images/next.png', 'next', array('style' => 'margin-left:5px;')), array('topic', 'id' => $model->lastPost->topic_id, 'nav' => 'last'));
 			echo Html::encode($model->lastPost->poster->member_name);
-			echo Html::a(Html::img($assets->baseUrl.'/images/next.png', 'next', array('style' => 'margin-left:5px;')), array('topic', 'id' => $model->lastPost->topic_id, 'nav' => 'last'));
-			echo '<br>';
-			echo Yii::$app->formatter->asDatetime($model->lastPost->create_time);
+            echo '<br>';
+
+            // Responive date formate display
+            $forumDateLong = Yii::$app->formatter->asDatetime($model->lastPost->create_time);
+            $forumDate = Yii::$app->formatter->asDatetime($model->lastPost->create_time, $format='php:M d, Y');
+            echo '<span class="hidden-xs">'.$forumDateLong.'</span>';
+            echo '<span class="visible-xs">'.$forumDate.'</span>';
+
 		} else {
 			echo Yii::t('BbiiModule.bbii', 'No posts');
 		}
