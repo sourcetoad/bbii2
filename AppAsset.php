711f06c0a2579af286df161db72cfe8816d9f50f
David E <me@davidjeddy.com>
2015-08-12 15:41:11 -0400

    FIXED issue where assets dir was not found via AppAsset class

--- AppAsset.php
+++ AppAsset.php
@@ -8,7 +8,7 @@ class AppAsset extends AssetBundle
 {
 	public $css 		= ['css/base/forum.css',];
 	public $js 			= ['js/bbii.js', 'js/bbiiSetting.js'];
-	public $sourcePath 	= '@app/modules/bbii/assets/';
+	public $sourcePath 	= '@vendor/sourcetoad/yii2-bbii2/assets/';
 
 	public $depends = [
 		'yii\web\YiiAsset', 
